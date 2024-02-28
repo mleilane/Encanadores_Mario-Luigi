20becd7ebdbefb593935de72bb74b4b5e2ac942a
mleilane <leilane94silva@yahoo.com>
2024-02-28 18:41:54 -0300

    Criamos a interação do cliente com o formulario, enviando os dados do orçamento para os responsaveis pelo serviço.

--- scripts.js
+++ scripts.js
@@ -5,11 +5,21 @@
 const formulario = document.querySelector(".formulario-fale-conosco")
 const mascara = document.querySelector(".mascara-formulario")
 
+//criando as funções que mudam o stylo do css conforme um evento é acionado
 //função para estilizar o formulario conforme clique no botão "fale conosco"
-function cliqueiNoBotao(){
+function mostrarForm() {
 
     formulario.style.left = "50%"
     formulario.style.transform = "translatex(-50%)"
     mascara.style.visibility = "visible"
 
 }
+
+//voltar os estilos anteriores da pagina, sem o formulario
+function esconderForm() {
+    formulario.style.left = "-300px"
+    formulario.style.transform = "translatex(0)"
+    mascara.style.visibility = "hidden"
+
+
+} px
